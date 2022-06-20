73a26a670be7f68f3502cd5f2ea39ab9950f4c85
Migueaali12 <malvarez11@utpl.edu.ec>
2022-06-20 14:38:36 -0500

    ....

--- EjemploHerencia/Herencias/src/herencia4/ReportePolicia.java
+++ EjemploHerencia/Herencias/src/herencia4/ReportePolicia.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package herencia4;
 
 import herencia2.Policia;
@@ -12,9 +7,43 @@ import java.util.ArrayList;
  *
  * @author reroes
  */
-public class ReportePolicia extends Reporte{
-    public ReportePolicia(String string) {
-    }
+public class ReportePolicia extends Reporte {
     double promedioEdades;
     ArrayList<Policia> lista;
+
+    public ReportePolicia(String cod) {
+        super(cod);
+
+    }
+
+    public void establecerLista(ArrayList<Policia> list) {
+        lista = list;
+    }
+
+    public void establecerEdades() {
+        double suma = 0;
+        for (int i = 0; i < lista.size(); i++) {
+            suma = suma + lista.get(i).getEdad();
+        }
+        promedioEdades = suma / lista.size();
+    }
+
+    public ArrayList<Policia> obtenerLista() {
+        return lista;
+    }
+
+    public double obtenerPromEdades() {
+        return promedioEdades;
+    }
+
+    @Override
+    public String toString() {
+        String cadena = String.format("%s\n", super.codigo);
+        for (int i = 0; i < lista.size(); i++) {
+            cadena = String.format("%s%s\n", cadena, lista.get(i));
+        }
+        cadena = String.format("%sPromedio de edades: %.2f\n ", cadena,
+                promedioEdades);
+        return cadena;
+    }
 }
